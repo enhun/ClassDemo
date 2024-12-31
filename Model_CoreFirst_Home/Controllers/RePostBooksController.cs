e0b066d86b575860b9cb095fc388b3b75cff4a36
Jane <cat35150123@gmail.com>
2024-12-31 21:12:51 +0800

    Initial commit

---
+++ Model_CoreFirst_Home/Controllers/RePostBooksController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model_CoreFirst_Home.Models;
+
+public class RePostBooksController : Controller
+{
+    private readonly GuestBookContext _context;
+
+    public RePostBooksController(GuestBookContext context)
+    {
+        _context = context;
+    }
+
+    // GET: RePostBooks/Create
+    public IActionResult Create(string bookId)
+    {
+        return PartialView(new ReBook { BookID = bookId });
+    }
+
+    // POST: RePostBooks/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([FromBody] ReBook reBook)
+    {
+        if (ModelState.IsValid)
+        {
+            reBook.ReBookID = Guid.NewGuid().ToString();
+            reBook.TimeStamp = DateTime.Now;
+
+            _context.Add(reBook);
+            await _context.SaveChangesAsync();
+
+            return Json(new
+            {
+                success = true,
+                data = new
+                {
+                    id = reBook.ReBookID,
+                    author = reBook.Author,
+                    description = reBook.Description,
+                    timestamp = reBook.TimeStamp.ToString("yyyy/MM/dd HH:mm:ss")
+                }
+            });
+        }
+        return Json(new { success = false, errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage) });
+    }
+}
---
+++ Model_CoreFirst_Home/ViewComponents/VCReBooks.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Model_CoreFirst_Home.Models;
+
+namespace MyModel_CodeFirst.ViewComponents
+{
+    public class VCReBooks : ViewComponent
+    {
+        private readonly GuestBookContext _context;
+
+        // 建構子，注入資料庫內容
+        public VCReBooks(GuestBookContext context)
+        {
+            _context = context;
+        }
+
+        // 這個方法會被自動調用來取得資料
+        public async Task<IViewComponentResult> InvokeAsync(string bookId)
+        {
+            var reBooks = await _context.ReBook
+                .Where(r => r.BookID == bookId)
+                .OrderByDescending(r => r.TimeStamp)
+                .ToListAsync();
+
+            return View(reBooks);  // 預設會找 Default.cshtml
+        }
+    }
+}
